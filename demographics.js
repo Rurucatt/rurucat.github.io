7443aae62d9c25efd19251895dd35a4d5be98210
Rurucatt <whiteeyuzu@gmail.com>
2026-02-18 02:10:38 -0500

    Update demographics.js

--- demographics.js
+++ demographics.js
@@ -1,23 +1,31 @@
 API.addQuestionsSet('demographics',[
-{
+  {
     type: 'text',
     name: 'age',
     stem: 'What is your age?'
-},
-{
+  },
+  {
     type: 'dropdown',
     name: 'gender',
     stem: 'Gender',
     answers: [
-        {text:'Female', value:'F'},
-        {text:'Male', value:'M'},
-        {text:'Non-binary', value:'NB'},
-        {text:'Prefer not to say', value:'NA'}
+      {text:'Female', value:'F'},
+      {text:'Male', value:'M'},
+      {text:'Non-binary', value:'NB'},
+      {text:'Prefer not to say', value:'NA'}
     ]
-},
-{
+  },
+  {
     type: 'text',
     name: 'major',
     stem: 'What is your major?'
-}
+  }
 ]);
+
+API.addTasks({
+  demographics: [{
+    type: 'quest',
+    name: 'demographics',
+    questionnaire: 'demographics'
+  }]
+});
