0e4d50b48e538dfb9aa8dd6b5f5701b421469c4a
Rurucatt <whiteeyuzu@gmail.com>
2026-02-18 02:58:35 -0500

    Update demographics.js

--- demographics.js
+++ demographics.js
@@ -1,34 +1,64 @@
-// demographics.js
-window.addDemographics = function(API){
-  API.addQuestionsSet('demographics',[
-    {
-      type: 'text',
-      name: 'age',
-      stem: 'What is your age?'
-    },
-    {
-      type: 'dropdown',
-      name: 'gender',
-      stem: 'Gender',
-      answers: [
-        {text:'Female', value:'F'},
-        {text:'Male', value:'M'},
-        {text:'Non-binary', value:'NB'},
-        {text:'Prefer not to say', value:'NA'}
-      ]
-    },
-    {
-      type: 'text',
-      name: 'major',
-      stem: 'What is your major?'
-    }
-  ]);
+define(['questAPI'], function(Quest){
+    let API = new Quest();
+    let isTouch = API.getGlobal().$isTouch;
 
-  API.addTasksSet('demographics', [
-    {
-      type: 'quest',
-      name: 'demographics',
-      questions: 'demographics'
-    }
-  ]);
-};
+    API.addPagesSet('demographicsPage', {
+        noSubmit: false,
+        header: 'Demographics',
+        decline: true,
+        declineText: isTouch ? 'Decline' : 'Decline to Answer',
+        autoFocus: true,
+        progressBar: 'Page <%= pagesMeta.number %> out of 1'
+    });
+
+    API.addQuestionsSet('demographicsQ', {
+        decline: 'true',
+        required: true,
+        autoSubmit: false,
+        errorMsg: {
+            required: isTouch
+                ? 'Please answer the question, or click \'Decline\''
+                : 'Please answer the question, or click \'Decline to Answer\''
+        }
+    });
+
+    API.addQuestionsSet('age', {
+        inherit: 'demographicsQ',
+        type: 'text',
+        name: 'age',
+        stem: 'What is your age?'
+    });
+
+    API.addQuestionsSet('gender', {
+        inherit: 'demographicsQ',
+        type: 'dropdown',
+        name: 'gender',
+        stem: 'Gender',
+        answers: [
+            {text: 'Female', value: 'F'},
+            {text: 'Male', value: 'M'},
+            {text: 'Non-binary', value: 'NB'},
+            {text: 'Prefer not to say', value: 'NA'}
+        ]
+    });
+
+    API.addQuestionsSet('major', {
+        inherit: 'demographicsQ',
+        type: 'text',
+        name: 'major',
+        stem: 'What is your major?'
+    });
+
+    API.addSequence([
+        {
+            inherit: 'demographicsPage',
+            questions: [
+                {inherit: 'age'},
+                {inherit: 'gender'},
+                {inherit: 'major'}
+            ]
+        }
+    ]);
+
+    return API.script;
+});
